2940450d61543565db793625948c1664f3415f8b
zishan344 <zishanahmed344@gmail.com>
2024-03-20 12:57:55 +0600

    added env file

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
     template: '%s | Acme Dashboard',
     default: 'Acme Dashboard',
   },
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  description: 'The official Next.js Course Dashboard,  built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 
